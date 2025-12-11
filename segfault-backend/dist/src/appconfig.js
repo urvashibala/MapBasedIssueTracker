3a1242ca715a307315028c75cb1ce6ee65addd58
Armaan Shah <armaan.shah2004@gmail.com>
2025-12-12 04:19:57 +0530

    fixed links 2

--- segfault-backend/dist/src/appconfig.js
+++ segfault-backend/dist/src/appconfig.js
@@ -1,8 +1,8 @@
-export const FRONTEND_URL = process.env.FRONTEND_URL ?? 'https://segfault-frontend.politeriver-a25e3b65.westeurope.azurecontainerapps.io';
+export const FRONTEND_URL = 'https://segfault-frontend.politeriver-a25e3b65.westeurope.azurecontainerapps.io';
 export const GOOGLE_OAUTH = {
     CLIENT_ID: process.env.GOOGLE_CLIENT_ID ?? '1030819278738-1fnklv7adb14ktovidi3ate2rlbhbe53.apps.googleusercontent.com',
     CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET ?? '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',
-    REDIRECT_URI: process.env.GOOGLE_REDIRECT_URI ?? 'https://segfault-backend.politeriver-a25e3b65.westeurope.azurecontainerapps.io/auth/callback',
+    REDIRECT_URI: 'https://segfault-backend.politeriver-a25e3b65.westeurope.azurecontainerapps.io/auth/callback',
 };
 export const JWT = {
     SECRET: process.env.JWT_SECRET ?? 'CHANGE_THIS_JWT_SECRET',
